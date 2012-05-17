640f50809f4a9160a0fdb460dca932bd17bb9654
davidsj <davidsj@02cd82ba-75cb-460b-9adb-b549677ef898>
2012-05-17 04:22:21 +0000

    significant UI improvement

--- dvs-jc/app/controllers/jc_absences_controller.rb
+++ dvs-jc/app/controllers/jc_absences_controller.rb
@@ -4,4 +4,11 @@ class JcAbsencesController < ApplicationController
     @jc_absence = @student.jc_absences.create(params[:jc_absence])
     redirect_to student_path(@student)
   end
+ 
+  def destroy
+    @student = Student.find(params[:student_id])
+    @jc_absence = @student.jc_absences.find(params[:id])
+    @jc_absence.destroy
+    redirect_to student_path(@student)
+  end
 end
--- dvs-jc/app/controllers/jc_substitutions_controller.rb
+++ dvs-jc/app/controllers/jc_substitutions_controller.rb
@@ -4,4 +4,11 @@ class JcSubstitutionsController < ApplicationController
     @jc_substitution = @student.jc_substitutions.create(params[:jc_substitution])
     redirect_to student_path(@student)
   end
+ 
+  def destroy
+    @student = Student.find(params[:student_id])
+    @jc_substitution = @student.jc_substitutions.find(params[:id])
+    @jc_substitution.destroy
+    redirect_to student_path(@student)
+  end
 end
