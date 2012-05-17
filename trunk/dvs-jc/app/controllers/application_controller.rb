f6133b0cfa9e87990b08ce3f8366480a3ce6d25e
davidsj <davidsj@02cd82ba-75cb-460b-9adb-b549677ef898>
2012-05-17 04:39:53 +0000

    authentication

--- trunk/dvs-jc/app/controllers/application_controller.rb
+++ trunk/dvs-jc/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
+# -*- coding: undecided -*-
+
+require 'dvs-password'
+# Make sure dvs-password.rb exists, with a secure password, e.g.:
+# 
+#     module DvsPassword
+#       def self.password
+#         return "p@$$w0rD" # choose a different password than this :)
+#       end
+#     end
+
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => "dvs", :password => DvsPassword.password
+
   protect_from_forgery
 end
